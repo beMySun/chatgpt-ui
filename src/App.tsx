53aa2a6189b523059f7e1c4e1692d2a06a24b978
皮小蛋 <917223209@qq.com>
2023-03-16 16:34:49 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {Flex} from '@chakra-ui/react';
 import { extendTheme, ChakraBaseProvider } from '@chakra-ui/react'
 import ChatWindow from './ChatWindow';
-import cat from './assets/1.png';
 
 const theme = extendTheme({
   styles: {
@@ -18,11 +17,6 @@ function App() {
     <ChakraBaseProvider theme={theme}>
       <Flex h="95vh" w="100vw" alignItems="center" justifyContent="center">
         <ChatWindow userId="1" />
-        <div className='cat' style={{ textAlign: "center"}}>
-          <img src={cat} alt="" />
-          <p> 谁是本群最可爱的女生 ？</p>
-          <p> 啊, 原来是董</p>
-        </div>
       </Flex>
     </ChakraBaseProvider>
   )
